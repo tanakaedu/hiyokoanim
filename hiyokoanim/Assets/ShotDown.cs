a46d7b1d0c64aed2ba20cece4fb850c8acd3f555
YuTanaka <tanaka@am1.jp>
2015-12-04 16:07:16 +0900

    2Dアニメ+卵発射

---
+++ hiyokoanim/Assets/ShotDown.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotDown : MonoBehaviour {
+	public GameObject prefShot = null;
+
+	public void shotDown() {
+		Instantiate(prefShot, transform.position, Quaternion.identity);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
